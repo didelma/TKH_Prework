9592fac2960629e7472dcf9ad14d304e9e8a431e
Jenny M <55765798+didelma@users.noreply.github.com>
2021-05-31 23:21:27 -0400

    Add files via upload

---
+++ Assignment_6.js
@@ -0,0 +1,11 @@
+//var names = ['Retta', 'Vern', 'Marica', 'Cyrus', 'Linette', 'Curt', 'Nichole', 'Jesica', 'Wynell', 'Michaele']
+
+//console.log(names[0]);
+
+//let sayHello = function () {
+  //  console.log();
+
+//}
+
+//names.forEach(sayHello)
+
