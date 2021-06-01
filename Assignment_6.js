01a94102eecae84d132c170b1724815f5c2b409a
Jenny M <55765798+didelma@users.noreply.github.com>
2021-06-01 00:00:53 -0400

    Add files via upload

--- Assignment_6.js
+++ Assignment_6.js
@@ -1,11 +1,5 @@
-//var names = ['Retta', 'Vern', 'Marica', 'Cyrus', 'Linette', 'Curt', 'Nichole', 'Jesica', 'Wynell', 'Michaele']
+var longest_name = ['Retta', 'Vern', 'Marica', 'Cyrus', 'Linette', 'Curt', 'Nichole', 'Jesica', 'Wynell', 'Michaele']
 
-//console.log(names[0]);
-
-//let sayHello = function () {
-  //  console.log();
-
-//}
-
-//names.forEach(sayHello)
-
+names.forEach(function(longest_name){
+document.write(longest_name + "<br>")
+}); 
