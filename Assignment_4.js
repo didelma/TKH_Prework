cfd52bf166bf1f3189cda0000949bd283c2a3d0b
Jenny M <55765798+didelma@users.noreply.github.com>
2021-05-31 21:17:23 -0400

    Add files via upload

--- Assignment_4.js
+++ Assignment_4.js
@@ -1,7 +1,11 @@
+
 var over_user_array = [1,45,32,21,5,43,93]
-if (over_user_array > 25){
-    print ("over")
-}
-else if (over_user_array < 25){
-    print ("under")
-};
+//i++ (start emission); i =0 loop begins; i<# amount of times.
+for(var i =0;i<7;i++){
+if (over_user_array [i] > 25){
+    console.log("over")}
+
+else if (over_user_array[i]< 25)
+{ console.log("under")
+
+}};
