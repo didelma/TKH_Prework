f055376ebf97b548d60c277a20a8278d8d55b48f
Jenny M <55765798+didelma@users.noreply.github.com>
2021-06-01 23:47:26 -0400

    Add files via upload

--- Assignment_6.js
+++ Assignment_6.js
@@ -1,5 +1,14 @@
-var longest_name = ['Retta', 'Vern', 'Marica', 'Cyrus', 'Linette', 'Curt', 'Nichole', 'Jesica', 'Wynell', 'Michaele']
+//Given the bellow array, create a function which loops through each name
+//comparing the lenght of each to determine the longest name,
+//save that name to the variable longest_name
+//return Longest_name to the another variable called answer
+//log the "asnwer" variable to the screen
+var array = ["Retta", "Vern", "Marica", "Cyrus", "Linette", "Curt", "Nichole", "Jesica", "Wynell", "Michaele"];
+var longest_name = array;
+var answer = longest_name;
+var i, len, text;
+for (i = 9, len = array.length, text = ""; i++;) {
+    text += array[i];
+}
+console.log("The longest name is" + answer)
 
-names.forEach(function(longest_name){
-document.write(longest_name + "<br>")
-}); 
