c6f65b7b4f0c0d483f2a5faf3b4ebc89ce483f43
Jenny M <55765798+didelma@users.noreply.github.com>
2021-06-05 23:35:14 -0400

    Add files via upload

--- Assignment_7.js
+++ Assignment_7.js
@@ -1,7 +1,21 @@
-var thing = "car"
-thing[0]
-thing[2]
-array = ["one",2,"3hree"]
-array[2]
+//Have js print a list of "even" names as A and "odd" names and C
+//
+//var thing = "car" 
+//thing[0]//c 
+//thing[2]//r 
+//array = ["one",2,"3hree"] 
+//array[2] // 3hree
 
-var names_array = ["bob", "jimmy", "max b", "bernie", "jordan", "future hendrix"]
+const names = ['bob', 'jimmy', 'max b', 'bernie', 'jordan', 'future hendrix'];
+
+var even_names = ['bob', 'bax b'];
+
+var odd_names = ['bernic', 'jimmc', 'jordac', 'future hendric'];
+
+const idx = names.indexOf(even_names) 
+
+names[idx] = ' '
+
+var ever_array = even_names
+
+console.log(even_names.indexOf('bax b'))
